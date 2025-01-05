89f6df4ac04b47ac5f9e11ecebc56df3e4abf88b
Nguyen Ngoc Bich <107031161+dhrqur@users.noreply.github.com>
2025-01-05 12:53:04 +0700

    Update product.php

--- user/product.php
+++ user/product.php
@@ -1,3 +1,47 @@
+<?php
+require('connect.php');
+
+// Mặc định hiển thị tất cả sản phẩm
+$sql = "SELECT * FROM sanpham";
+
+// Lọc theo giới tính (gender)
+if (isset($_GET['gender'])) {
+    $gender = $_GET['gender'];
+    if ($gender === "All") {
+        $sql = "SELECT * FROM sanpham WHERE gender IN (0, 1, 2)";
+    } elseif ($gender === "Nam") {
+        $sql = "SELECT * FROM sanpham WHERE gender = 0 OR gender = 2";
+    } elseif ($gender === "Nữ") {
+        $sql = "SELECT * FROM sanpham WHERE gender = 1 OR gender = 2";
+    }
+}
+
+// Lọc theo tìm kiếm (search)
+if (isset($_GET['search'])) {
+    $noidung = $conn->real_escape_string($_GET['noidung']);
+    $sql = "SELECT * FROM sanpham WHERE name LIKE '%$noidung%'";
+}
+
+// Lọc theo dòng sản phẩm (family)
+if (isset($_GET['family'])) {
+    $gender = $_GET['family'];
+    if ($gender === "Basas") {
+        $sql = "SELECT * FROM sanpham WHERE family = 0";
+    } elseif ($gender === "Vintas") {
+        $sql = "SELECT * FROM sanpham WHERE family = 1";
+    } elseif ($gender === "Urbas") {
+        $sql = "SELECT * FROM sanpham WHERE family = 2";   
+    } elseif ($gender === "Pattas") {
+        $sql = "SELECT * FROM sanpham WHERE family = 3";   
+    } elseif ($gender === "Track6") {
+        $sql = "SELECT * FROM sanpham WHERE family = 4";   
+    }
+}
+
+// Thực thi câu truy vấn
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,136 +51,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="logo/Logo_Ananas.svg">
     <title>PRODUCT</title>
-    <!-- ====================================font-awesonme========= -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- ============css==============s -->
-    <link rel="stylesheet" href="css/product.css">
-
+    <link rel="stylesheet" href="./css/product-list.css">
 </head>
 
 <body>
-<!--ĐÂY LÀ PHẦN HEADER-->
-<?php include 'header.php'; ?>
-    
-    <main class="product">
-            <!-- Sản phẩm -->
+    <!-- HEADER -->
+    <?php include 'header.php'; ?>
+
+    <!-- MAIN CONTENT -->
+    <main class="main-content">
+        <div class="container">
+            <!-- Sidebar (Menu tùy chọn) -->
+            <div class="product-nav">
+                <h3>TÙY CHỌN</h3>
+                    <div class="category">
+                        <h4>DÀNH CHO</h4>
+                        <ul>
+                            <li><a href="?gender=All">Tất Cả</a></li>
+                            <li><a href="?gender=Nam">Nam</a></li>
+                            <li><a href="?gender=Nữ">Nữ</a></li>
+                        </ul>
+                    </div>
+                    <div class="category">
+                        <h4>DÒNG SẢN PHẨM</h4>
+                        <ul>
+                            <li><a href="?family=Basas">Basas</a></li>
+                            <li><a href="?family=Vintas">Vintas</a></li>
+                            <li><a href="?family=Urbas">Urbas</a></li>
+                            <li><a href="?family=Pattas">Pattas</a></li>
+                            <li><a href="?family=Track6">Track 6</a></li>
+                        </ul>
+                    </div>
+            </div>
+
+            <!-- Product List -->
             <div class="product-list">
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP1.jpg" alt="Sản phẩm 1">
-                        <h4>Basas Bumper Gum EXT NE - Low Top</h4><br>
-                        <p>580.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP2.jpg" alt="Sản phẩm 2">
-                    <h4>Basas Bumper Gum EXT NE - High Top</h4><br>
-                    <p>650.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP3.jpg" alt="Sản phẩm 3">
-                        <h4>Basas Day Slide - Slip On</h4><br>
-                        <p>550.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP4.jpg" alt="Sản phẩm 4">
-                        <h4>Basas Evergreen - Low Top</h4><br>
-                        <p>580.000 VND</p>
-                    </a>
-                </div>
-    
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP5.jpg" alt="Sản phẩm 5">
-                    <h4>Vintas Denim - High Top</h4><br>
-                    <p>690.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP6.jpeg" alt="Sản phẩm 6">
-                        <h4>Vintas Vivu - Low Top</h4><br>
-                        <p>620.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP7.jpg" alt="Sản phẩm 7">
-                    <h4>Vintas Public 2000s - Low Top</h4><br>
-                    <p>620.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP8.jpeg" alt="Sản phẩm 8">
-                        <h4>Vintas Jazico - High Top</h4><br>
-                        <p>780.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP9.jpg" alt="Sản phẩm 9">
-                    <h4>Pattas Polka Dots - High Top</h4><br>
-                    <p>390.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP10.jpeg" alt="Sản phẩm 10">
-                        <h4>Basas Bumper Gum NE - Mule</h4><br>
-                        <p>520.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP11.jpeg" alt="Sản phẩm 11">
-                        <h4>Basas Hook N'Loop NE - Mule</h4><br>
-                        <p>520.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP12.jpg" alt="Sản phẩm 12">
-                    <h4>Track 6 Utility Gum Sole - Low Top</h4><br>
-                    <p>1.090.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                        <img src="img/SP13.jpg" alt="Sản phẩm 13">
-                        <h4>Track 6 Triple White - Low Top</h4><br>
-                        <p>990.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP14.jpg" alt="Sản phẩm 14">
-                    <h4>Track 6 Class E - Low Top</h4><br>
-                    <p>1.990.000 VND</p>
-                    </a>
-                </div>
-                <div class="product-item">
-                    <a href="product-detail.html">
-                    <img src="img/SP15.jpg" alt="Sản phẩm 15">
-                    <h4>Basas Mono Black NE -  Low Top</h4><br>
-                    <p>490.000 VND</p>
-                    </a>
-                </div>
-                
+                <?php while ($product = $result->fetch_assoc()): ?>
+                    <div class="product-item">
+                        <a href="product-detail.php?id=<?php echo $product['id'] ?>">
+                            <img src="<?php echo $product['img'] ?>" alt="<?php echo $product['name'] ?>">
+                        </a>
+                        <h2 class="product-name">
+                            <a href="product-detail.php?id=<?php echo $product['id'] ?>">
+                                <?php echo $product['name'] ?>
+                            </a>
+                        </h2>
+                        <p class="product-price"><?php echo number_format($product['price'], 0, ',', ',') . " VND"; ?></p>
+                        <a href="detail-products.php?id=<?php echo $product['id'] ?>" class="product-button">Thêm vào giỏ hàng</a>
+                    </div>
+                <?php endwhile; ?>
             </div>
         </div>
     </main>
-<!--ĐÂY LÀ PHẦN FOOTER-->
-<?php include 'footer.php'; ?>
-<!--ĐÂY LÀ PHẦN BÊN TAY PHẢI-->
-<?php include 'cart_right.php'; ?>
+
+    <!-- FOOTER -->
+    <?php include 'footer.php'; ?>
+    <?php include 'cart_right.php'; ?>
+
 </body>
 
 </html>
